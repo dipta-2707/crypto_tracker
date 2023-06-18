a8f8cae67175273d361acefed2824e0affd34ee5
dipta-increments <58099256+dipta-2707@users.noreply.github.com>
2023-06-18 14:29:45 +0600

    Update theme_config.dart

--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppThemeConfig {
   static final mainTheme = ThemeData(
-    useMaterial3: true,
-    fontFamily: 'SpaceMono',
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-  );
+      useMaterial3: true,
+      fontFamily: 'SpaceMono',
+      brightness: Brightness.dark,
+      outlinedButtonTheme: OutlinedButtonThemeData(
+          style: ButtonStyle(
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.zero),
+      )));
 }
