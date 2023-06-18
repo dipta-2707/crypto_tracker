fedd6e3b13535ab95eb60586a9d32972efc78c11
dipta-increments <58099256+dipta-2707@users.noreply.github.com>
2023-06-18 12:50:06 +0600

    space mono text add

--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppThemeConfig {
   static final mainTheme = ThemeData(
     useMaterial3: true,
+    fontFamily: 'SpaceMono',
     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
   );
 }
--- lib/view/widgets/crypto_list_tile.dart
+++ lib/view/widgets/crypto_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class CryptoListTile extends StatelessWidget {
   const CryptoListTile({Key? key}) : super(key: key);
@@ -17,12 +18,21 @@ class CryptoListTile extends StatelessWidget {
         errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
       title: Text('Crypto Name'),
-      subtitle: Text('Crypto Symbol'),
+      subtitle: Text(
+        'Crypto Symbol',
+        style: Get.textTheme.bodySmall,
+      ),
       trailing: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Crypto Price'),
-          Text('Crypto Change %'),
+          Text(
+            'Crypto Price',
+            style: Get.textTheme.bodyMedium,
+          ),
+          Text(
+            'Crypto Change %',
+          ),
         ],
       ),
       onTap: () {},
